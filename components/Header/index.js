ea8615a17ac04948136b5ab787190449d3610b14
Adam Soffer <adam@soffer.dev>
2019-08-23 20:57:11 -0400

    Update deps

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,34 +6,52 @@ const Header = () => {
   const router = useRouter();
   // Have to check if router exists until this issue is resolved
   // https://github.com/zeit/next.js/issues/6042
-  if (router) {
-    const { pathname } = router;
-    return (
-      <Container>
-        <Link prefetch href="/">
-          <a>
-            <LinkText isActive={pathname === "/" && "is-active"}>Home</LinkText>
-          </a>
-        </Link>
-
-        <Link prefetch href="/about">
-          <a>
-            <LinkText isActive={pathname === "/about" && "is-active"}>
-              About
-            </LinkText>
-          </a>
-        </Link>
-
-        <Link href="https://github.com/ads1018/next-apollo-example">
-          <a>
-            <LinkText>Github</LinkText>
-          </a>
-        </Link>
-      </Container>
-    );
-  } else {
+  if (!router) {
     return null;
   }
+  const { pathname } = router;
+  return (
+    <Container>
+      <Link href="/">
+        <a>
+          <LinkText isActive={pathname === "/" && "is-active"}>Home</LinkText>
+        </a>
+      </Link>
+      <Link href="/about">
+        <a>
+          <LinkText isActive={pathname === "/about" && "is-active"}>
+            About
+          </LinkText>
+        </a>
+      </Link>
+      <a
+        href="https://github.com/ads1018/next-apollo-example"
+        target="__blank"
+        rel="noopener noreferrer"
+      >
+        <LinkText>Github</LinkText>
+      </a>
+    </Container>
+  );
+
+  // const { pathname } = router
+  // return (
+  //   <Container>
+  //     <Link href="/">
+  //       <a>
+  //         <LinkText isActive={pathname === '/' && 'is-active'}>Home</LinkText>
+  //       </a>
+  //     </Link>
+
+  //     <Link href="/about">
+  //       <a>
+  //         <LinkText isActive={pathname === '/about' && 'is-active'}>
+  //           About
+  //         </LinkText>
+  //       </a>
+  //     </Link>
+
+  // )
 };
 
 export default Header;
